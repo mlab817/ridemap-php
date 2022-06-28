28807b9e47d059790bfe1e6d5a117d8fc1cad981
mlab817 <mlab817@gmail.com>
2022-06-28 19:19:35 +0800

    updated title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <title>{{ config('app.name', 'Ridemap') }}</title>
+
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
