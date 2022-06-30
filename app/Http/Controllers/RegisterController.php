0140a90315bebcae4b9dd9581976b6807b18a836
mlab817 <mlab817@gmail.com>
2022-06-30 08:45:57 +0800

    updated user registration logic to use hashed password

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
@@ -16,14 +17,19 @@ class RegisterController extends Controller
 
     public function registerUser(Request $request)
     {
-        $user = User::create(
-            $request->validate([
-                'name'      => ['required', 'max:50'],
-                'email'     => ['required', 'max:50', 'email', 'unique:users,email'],
-                'password'  => ['required'],
-                'device_id' => ['required']
-            ])
-        );
+        $request->validate([
+            'name'      => ['required', 'max:50'],
+            'email'     => ['required', 'max:50', 'email', 'unique:users,email'],
+            'password'  => ['required'],
+            'device_id' => ['required']
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email'=> $request->email,
+            'password' => Hash::make($request->password),
+            'device_id' => $request->device_id,
+        ]);
 
         return Redirect::route('register.showForm')
             ->with('message','Successfully registered user: ' . $user->name);
